0da97565f5fc367ac87dce7e164b13d4ff169771
statya272 <statya272@mail.ru>
2022-05-09 17:14:26 +0500

    First commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,61 @@
+public class Cat {
+    private String id;
+    private String text;
+    private String type;
+    private String user;
+    private String upvotes;
+
+    public Cat() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getUpvotes() {
+        return upvotes;
+    }
+
+    public void setUpvotes(String upvotes) {
+        this.upvotes = upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "id='" + id + '\'' +
+                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                ", upvotes='" + upvotes + '\'' +
+                '}';
+    }
+}
